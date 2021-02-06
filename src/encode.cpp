b2a99702cb9ff3812ff7f783e43807ca600d39a7
Diego Guerrero <tembio@gmail.com>
2021-02-06 18:13:02 +0000

    Add comments to clarify encoder

--- src/encode.cpp
+++ src/encode.cpp
@@ -19,40 +19,60 @@ void scan(char* input, int length) {
 }
 
 int main() {
+
+    // READ FROM CIN -> INPUT
     char* input = new char[1024];
-    int i;
+    int i; //number of read chars
     for (i = 0;; i++) {
         std::cin.get(input[i]);
         if (std::cin.eof())
             break;
     }
 
+    // CREATE SYMBOL TABLE
     symbol_table = new char[1024];
     scan(input, i);
 
-    std::cout.put(symbol_table_length);
-    std::cout.write(symbol_table, symbol_table_length);
 
+
+    //Print number of symbols
+    std::cout.put(symbol_table_length);
+    //Print table of symbols
+    std::cout.write(symbol_table, symbol_table_length);
+    //Print number read chars+1
     std::cout.put(i);
 
-    char out = -1;
-    for (int j = 0; j < i; j++) {
-        int k;
-        for (k = 0; k < symbol_table_length; k++) {
+
+
+    // OUPTUT COMPACT REPRESENTATION
+    char out = -1; //Temp var to print stuff (2 symbols per char)
+
+    for (int j = 0; j < i; j++) { // Foreach char read
+
+        // FIND input  in SYMBOL TABLE
+        int k; // INDEX IN SYMBOL TABLE
+        for (k = 0; k < symbol_table_length; k++) { //if you find it in the symbol table (stop looking)
             if (symbol_table[k] == input[j]) {
                 break;
             }
         }
-        if (out == -1) {
-            out = k & 0xf;
+
+
+        if (out == -1) {  // Store in less significative?
+
+            out = k & 0xf; // Store the index of the current item in the least significative half of the byte
             if (j == i - 1) {
                 std::cout.put(out);
             }
+
         } else {
-            out <<= 4;
-            out |= k & 0xf;
+
+            out <<= 4;     // move the prev index to the most significative part of the byte
+            out |= k & 0xf; // Store the index of the current item in the least significative half of the byte
             std::cout.put(out);
-            out = -1;
+
+            out = -1; // set all bits to 1
+
         }
     }
     return 0;
