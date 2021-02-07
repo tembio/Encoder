40c360a8ad95ad1223474d53f9ed5eebb9f9c33d
Diego Guerrero <tembio@gmail.com>
2021-02-07 20:16:38 +0000

    Create main for decoder

--- src/decode.cpp
+++ src/decode.cpp
@@ -1,27 +1,12 @@
-// #include <iostream>
+#include <iostream>
+#include "decoder.hpp"
 
-// char* symbol_table;
-// char symbol_table_length = 0;
+int main() {
 
-// int main() {
-//     symbol_table = new char[1024];
-//     std::cin.get(symbol_table_length);
-//     std::cin.read(symbol_table, symbol_table_length);
+    Decoder decoder;
 
-//     char len;
-//     std::cin.get(len);
+    std::cin >> decoder;
+    std::cout << decoder;
 
-//     for (int i = 0;; i += 2) {
-//         char in;
-//         std::cin.get(in);
-//         if (std::cin.eof())
-//             break;
-//         if (i == len - 1) {
-//             std::cout.put(symbol_table[in & 0xf]);
-//         } else {
-//             std::cout.put(symbol_table[(in >> 4) & 0xf]);
-//             std::cout.put(symbol_table[in & 0xf]);
-//         }
-//     }
-//     return 0;
-// }
+    return 0;
+}
