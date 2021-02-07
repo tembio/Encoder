ed331bb500fff1b9aaffdbc8e717ba4e779120b2
Diego Guerrero <tembio@gmail.com>
2021-02-07 16:57:16 +0000

    Write tests for operator<<

--- test/encoder_test.cpp
+++ test/encoder_test.cpp
@@ -73,3 +73,51 @@ TEST(Encoder, GetEncodedDataReturnsEncodedDataWhenInputHasBeenRead) {
     EXPECT_EQ (output.str().c_str()[1], '\x20');
     EXPECT_EQ (output.str().c_str()[2], '\x02');
 }
+
+TEST(Encoder, WriteToOutputIsCorrect) {
+    // Given
+    Encoder encoder;
+    std::stringstream input;
+    input << "abcac";
+    input >> encoder;
+
+    // When 
+    std::stringstream output;
+    output << encoder;
+
+    // Then
+    EXPECT_EQ (output.str().c_str()[0], 3);
+    EXPECT_EQ (output.str().c_str()[1], 'a');
+    EXPECT_EQ (output.str().c_str()[2], 'b');
+    EXPECT_EQ (output.str().c_str()[3], 'c');
+    EXPECT_EQ (output.str().c_str()[4], '\x01');
+    EXPECT_EQ (output.str().c_str()[5], '\x20');
+    EXPECT_EQ (output.str().c_str()[6], '\x02');
+}
+
+
+TEST(Encoder, WriteToOutputIsCorrectForGivenExample) {
+    // Given
+    Encoder encoder;
+    std::stringstream input;
+    input << "RYKARRKAAGCWATYWKMTCTWRAGSRMGCTKGTKCACMYACYTTMG";
+    input >> encoder;
+
+    // When 
+    std::stringstream output;
+    output << encoder;
+
+    // Then
+    std::vector<char> expectedChars {
+        10,
+        'R','Y', 'K', 'A', 'G', 'C', 'W', 'T', 'M', 'S',
+        '\x01', '\x23', '\x00', '\x23', '\x34', '\x56', '\x37',
+        '\x16', '\x28', '\x75', '\x76', '\x03', '\x49', '\x08',
+        '\x45', '\x72', '\x47', '\x25', '\x35', '\x81', '\x35',
+        '\x17', '\x78', '\x04'};
+
+    auto index = 0;
+    for (auto expected : expectedChars) {
+        EXPECT_EQ (output.str().c_str()[index++], expected);
+    }    
+}
